43a8fb499e4a30f02069c78bbf97eb4ad073da1a
Thillai kannan <sthillaikannan2003.gmail.com>
2023-07-07 14:23:56 +0530

    first commit

---
+++ client/src/firebase.config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD0o_F_AVOagDnWXJUwQTdyasmMUwwjV2M",
+  authDomain: "stackoverflow-25e60.firebaseapp.com",
+  projectId: "stackoverflow-25e60",
+  storageBucket: "stackoverflow-25e60.appspot.com",
+  messagingSenderId: "347857631324",
+  appId: "1:347857631324:web:71ff295bb90b1f29174e99"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
